6c88a6f3856f78218d3de3aa292625d018d1ca0c
Amanda Moraes <amandascm23@gmail.com>
2023-11-09 01:15:40 -0300

    fix(getSourceFileCorrespondingLine): instead of picking the start line, prioritize the end line

--- src/override_assignment/utils.js
+++ src/override_assignment/utils.js
@@ -1,6 +1,6 @@
 function getSourceFileCorrespondingLine (location) {
     // location in the format: file_path.js:15:25:15:28
-    return Number(location.split(':')[1])
+    return Number(location.split(':')[3])
 }
 
 module.exports = {
